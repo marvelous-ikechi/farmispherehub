d9d0801d65eb5271821726b4f936e8dcd16e97cd
marvelous <marvelike7@gmail.com>
2021-08-26 00:03:08 +0100

    :sparkles: notion database added to endpoint

---
+++ pages/api/submit.js
@@ -0,0 +1,80 @@
+const { Client } = require('@notionhq/client');
+
+const notion = new Client({
+  auth: process.env.NOTION_API_KEY,
+});
+
+const handler = async (req, res) => {
+
+  // connect to database
+  const response = await notion.databases.retrieve({database_id:process.env.NOTION_DATABASE_ID})
+  if (req.method !== 'POST'){
+    return res
+      .status(405)
+      .json({
+        message:`${req.method} requests are not allowed`
+      });
+  }
+  try {
+    const { first_name, last_name, farm_name, farm_type, farm_size, products } = req.body;
+    await notion.pages.create({
+      parent: {
+        database_id: process.env.NOTION_DATABASE_ID,
+      },
+      properties: {
+        first_name: {
+          rich_text: [
+            {
+              text: {
+                content: first_name,
+              },
+            },
+          ],
+          
+        },
+        last_name: {
+          rich_text: [
+            {
+              text: {
+                content: last_name,
+              },
+            },
+          ],
+        },
+        farm_name: {
+          rich_text: [
+            {
+              text: {
+                content: farm_name,
+              },
+            },
+          ],
+        },
+        farm_type: {
+          select: {
+            name: farm_type,
+          },
+        },
+        farm_size: {
+          select: {
+            name: farm_size,
+          },
+        },
+        products: {
+          rich_text: [
+            {
+              text: {
+                content: products,
+              },
+            },
+          ],
+        },
+      },
+    });
+    res.status(201).json({ msg: 'Success' });
+  } catch (error) { 
+    res.status(500).json({ msg: 'There was an error' });
+  }
+}
+
+export default handler
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,46 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
+import axios from 'axios'
 
 export default function Home() {
+  const [first_name, setFirstName] = useState('');
+  const [last_name, setLastName] = useState('');
+  const [farm_name, setFarmName] = useState('');
+  const [farm_type, setFarmType] = useState('');
+  const [farm_size, setFarmSize] = useState('');
+  const [products, setProducts] = useState('');
+
+
+  // Submit handler
+  const submitHandler = async (event) =>{
+    event.preventDefault();
+    await axios.post('http://localhost:3000/api/submit', {
+      first_name, last_name, farm_name, farm_type, farm_size, products
+      })
+      .then(
+        response =>{
+          console.log(response.data);
+          if (response.status === 201){
+              alert("Registration was successful");
+              setFirstName('');
+              setLastName('');
+              setFarmName('');
+              setFarmSize('');
+              setFarmType('');
+              setProducts('');
+          }
+        }
+      ).catch(
+        error => {
+          console.log(error);
+          toast('Please re-check all input fields', {type:'error'}) 
+        }
+      )
+  }
+
   return (
     <body>
       <Head>
@@ -10,34 +48,36 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com"  /> 
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet" />
       </Head>
+
+{/* background image */}
     <div className="form-container">
-     
+    
     </div>
-     <form>
+     <form onSubmit={submitHandler}>    
        <h3>Farmers Registration Form</h3>
          <div>
              <label>
                 First Name
              </label>
-             <input type="text" placeholder="John" />
+             <input type="text" placeholder="John" name="first_name" required value={first_name} onChange={(event) => setFirstName(event.target.value)}/>
          </div>
          <div >
              <label>
                Last  Name
              </label>
-             <input type="text" placeholder="Doe" />
+             <input type="text" placeholder="Doe" name="last_name" required value={last_name} onChange={(event) => setLastName(event.target.value)}/>
          </div>
          <div >
              <label>
                Farm  Name
              </label>
-             <input type="text" placeholder="Farm XYZ" />
+             <input type="text" placeholder="Farm XYZ" name="farm_name"  required value={farm_name} onChange={(event) => {setFarmName(event.target.value), console.log(event.target)}}/>
          </div>
          <div >
              <label>
                Farm  Type
              </label>
-             <select name="farm-type" id="farm-type">
+             <select name="farm-type" id="farm-type" name="farm_type" required value={farm_type} onChange={(event) => setFarmType(event.target.value)}>
                  <option value="fish">Fish Farm</option>
                  <option value="snail">Snail Farm</option>
                  <option value="poultry">Poultry</option>
@@ -48,7 +88,7 @@ export default function Home() {
              <label>
                Size of Farm
              </label>
-             <select name="farm-size" id="farm-size">
+             <select name="farm-size" id="farm-size" name="farm_size" value={farm_size} onChange={(event) => setFarmSize(event.target.value)} required>
                  <option value="1-10">1-10</option>
                  <option value="11-50">11-50</option>
                  <option value="51-100">51-100</option>
@@ -59,10 +99,10 @@ export default function Home() {
              <label>
                Products
              </label>
-             <input type="text" placeholder="Fish, Snail, chickens" />
+             <input type="text" placeholder="Fish, Snail, chickens" required name="products" value={products} onChange={(event) =>setProducts(event.target.value)}/>
          </div>
          <div className="submit-btn-div">
-             <button className="submit-btn">Submit</button>
+             <button className="submit-btn" type="submit">Register</button>
          </div>
    </form>
  </body>
